b292bcfc618d4e5cca2a6ba032e0dee19f55b706
Rommmah <rommah@ro.ru>
2022-07-02 20:49:37 +0300

    Сделал ДЗ №2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -3,6 +3,15 @@ function getArrayParams(arr) {
   let min, max, sum, avg;
 
   // Ваш код
+  sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    if (!min) min = arr[i]
+    if (!max) max = arr[i]
+    if (arr[i] < min) min = arr[i]
+    if (arr[i] > max) max = arr[i]
+    sum += arr[i]
+  }
+  avg = +(sum / arr.length).toFixed(2)
 
   return { min: min, max: max, avg: avg };
 }
@@ -12,6 +21,10 @@ function worker(arr) {
   let sum;
 
   // Ваш код
+  sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i]
+  }
 
   return sum;
 }
@@ -20,7 +33,11 @@ function makeWork(arrOfArr, func) {
   let max;
 
   // Ваш кода
-  // for ...
+  for(let i = 0; i < arrOfArr.length; i++){
+    let currentSum = func( arrOfArr[i] )
+    if (max === undefined) max = currentSum
+    if (currentSum > max) max = currentSum
+  }
   
   return max;
 }
@@ -28,4 +45,12 @@ function makeWork(arrOfArr, func) {
 // Задание 3
 function worker2(arr) {
   // Ваш код
+  let min, max;
+  for(let i = 0; i < arr.length; i++){
+    if (!min) min = arr[i]
+    if (!max) max = arr[i]
+    if (arr[i] < min) min = arr[i]
+    if (arr[i] > max) max = arr[i]
+  }
+  return Math.abs(max - min)
 }
