3f0ada205c80ebca20546261e652814e1869daea
Rommmah <rommah@ro.ru>
2022-07-11 21:12:05 +0300

    Сделал ДЗ№3 про массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,8 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  if(arr1.length !== arr2.length) return false
+  result = arr1.every( (item, idx) => arr1[idx] === arr2[idx] )
 
   return result; // boolean
 }
@@ -10,6 +12,7 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+  resultArr = arr.filter( item => item > 0 && item % 3 === 0).map( item => item * 10)
 
   return resultArr; // array
 }
